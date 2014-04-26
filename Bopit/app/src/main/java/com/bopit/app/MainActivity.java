2fc74ba2aabdff107153fa4941133861d254d1a2
juvs <juve.guz@gmail.com>
2014-04-25 21:46:23 -0500

    bajando

--- Bopit/app/src/main/java/com/bopit/app/MainActivity.java
+++ Bopit/app/src/main/java/com/bopit/app/MainActivity.java
@@ -1,23 +1,41 @@
 package com.bopit.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Button;
+
 
 
 public class MainActivity extends ActionBarActivity {
+    private int players = 0;
+    private ImageButton player1;
+    private ImageButton player2;
+    private ImageButton player3;
+    private ImageButton player4;
+    private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        player1 = (ImageButton) findViewById(R.id.P1);
+        player2 = (ImageButton) findViewById(R.id.P2);
+        player3 = (ImageButton) findViewById(R.id.P3);
+        player4 = (ImageButton) findViewById(R.id.P4);
+        //intent = new Intent(this,);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -35,4 +53,31 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onResume() {
+        player1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        player2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        player3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        player4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
